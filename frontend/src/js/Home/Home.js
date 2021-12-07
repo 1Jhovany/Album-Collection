c36668601f33cca5dcdc30826eb403b68d6036dd
1Jhovany <cuevasjhovany@gmail.com>
2021-12-07 10:28:38 -0600

    addedhome

---
+++ frontend/src/js/Home/Home.js
@@ -0,0 +1,18 @@
+import * as CONSTANTS from "../components/constants/"
+
+export default{
+    displayHome
+}
+
+function displayHome(){
+    return `
+    <h1 id="homeTitle">Welcome to Educational Game Maker
+    </h1>
+    <h3 id="homeSub">Directions: Choose any of the following games on the navbar to play and have fun.</h3>
+    <div class="container">
+    </br>
+        <div>
+        </div>
+    </div>
+    `
+}
